490973b0c62db369b7bd6539d8e0b4c1d2be391c
Gabriel Meireles <dev.meireles@gmail.com>
2023-11-03 11:49:53 -0300

    initial commit

--- app/main.go
+++ app/main.go
@@ -1,31 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"os"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello from Devcontainer")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
 	})
 
-	e.GET("/health", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("PORT")
-	if httpPort == "" {
-		httpPort = "3000"
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		fmt.Println("Server error:", err)
 	}
-
-	e.Logger.Fatal(e.Start(":" + httpPort))
 }
